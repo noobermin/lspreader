cc9a05a4899f88f235edf36ef3fa0ca5435de392
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-01-25 15:49:05 -0500

    adding to track

--- lspreader/lspreader.py
+++ lspreader/lspreader.py
@@ -251,6 +251,7 @@ def read(fname,**kw):
                     concatenation.
     '''
     openf = gzip.open if test(kw, 'gzip') else open;
+    print(openf);
     with openf(fname,'rb') as file:
         if test(kw,'override'):
             dump, start = kw['override'];
