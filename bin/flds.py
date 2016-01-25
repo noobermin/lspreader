4d2a99505eb19f6b89cf7b95036a48711603af67
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-01-25 17:27:17 -0500

    fixing flds script

--- bin/flds.py
+++ bin/flds.py
@@ -18,7 +18,7 @@ import numpy as np;
 from docopt import docopt;
 from lspreader.misc import dump_pickle,mkvprint;
 from lspreader import lspreader as rd;
-from lspreader.flds import rect;
+import lspreader.flds as flds;
 opts=docopt(__doc__,help=True);
 vprint = mkvprint(opts);
 if len(opts['<var>']) == 0:
@@ -32,7 +32,7 @@ d=rd.read(opts['<input>'],
 vprint("time to read file {}: {}".format(opts['<input>'],time()-b));
 vprint("read: {}".format(",".join(d.keys())));
 if opts['--reshape']:
-    d = rect_flds(d);
+    d = flds.sort(d,flds.firstsort(d))
 if opts['--pickle']:
     dump_pickle(opts['<output>'],d);
 else:
