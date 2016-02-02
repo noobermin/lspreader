e289a9c20b434552f13e704f5b690b044189b49d
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-02-02 15:31:22 -0800

    changed the scanning system

--- pmovie/scanp4.py
+++ pmovie/scanp4.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 '''
-Search a p4 for good indices.
+Search a p4 for good indices. This imports a file called
+scanner from the current directory, reads a function called "f"
+and runs this on each frame. It should return a numpy array of
+good trajectories.
 
 Usage:
     ./search.py [options] <input> <output>
@@ -9,23 +12,18 @@ Options:
     --help -h                    Print this help.
     --hash=HASHFILE -H HASHFILE  Read this hash file.
     --gzip -Z                    Read gzipped files.
-    --minE=E -e E                Give the minimum E in MeV. [default: 0.5].
-    --maxX=X -x X                Filter on position with energy [default: -10e-4].
 '''
 from lspreader import read;
 from lspreader.pmovie import addhash, filter_hashes_from_file;
 from lspreader.misc import readfile;
 import numpy as np;
 
+#expects a "scanner.py" file in the same directory
+#
+from scanner import f;
 if __name__ == "__main__":
     from docopt import docopt;
     opts=docopt(__doc__,help=True);
-    minE = float(opts['--minE']);
-    maxX = float(opts['--maxX']);
-    def f(frame):
-        data=frame['data'];
-        E   = (np.sqrt(data['ux']**2+data['uz']**2+1)-1)*0.511;
-        return np.logical_and(E>minE, data['x'] < maxX);
     hashd = readfile(opts['--hash'],dumpfull=True);
     np.save(
         opts['<output>'],
