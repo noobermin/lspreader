02e2d0b0d8fc8664c35ad77f3c3a77a421ba7483
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-02-02 13:40:44 -0800

    fixing scripts

--- pmovie/scanp4.py
+++ pmovie/scanp4.py
@@ -16,18 +16,16 @@ from lspreader import read;
 from lspreader.pmovie import addhash, filter_hashes_from_file;
 from lspreader.misc import readfile;
 import numpy as np;
-from time import time;
-
 
 if __name__ == "__main__":
     from docopt import docopt;
+    opts=docopt(__doc__,help=True);
     minE = float(opts['--minE']);
     maxX = float(opts['--maxX']);
     def f(frame):
         data=frame['data'];
         E   = (np.sqrt(data['ux']**2+data['uz']**2+1)-1)*0.511;
         return np.logical_and(E>minE, data['x'] < maxX);
-    opts=docopt(__doc__,help=True);
     hashd = readfile(opts['--hash'],dumpfull=True);
     np.save(
         opts['<output>'],
