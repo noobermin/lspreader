628a7233c7b424e7250776488577787994748f21
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-05-23 17:24:21 -0400

    committing to master out of laziness, yay!

--- bin/read.py
+++ bin/read.py
@@ -1,12 +1,22 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 '''
 Extract data directly.
 
-Usage: read.py <input> <output>
+Usage: 
+    read.py [options] <input> <output>
+
+Options:
+    --npy -n          Output to npy instead of pickle.
+    --pickle -p       Output to pickle independent of file ending of output.
 '''
-from lspreader import read
-from lspreader.misc import dump_pickle;
+from lspreader import read;
+from pys import dump_pickle;
 from docopt import docopt;
+import numpy as np;
+import re;
 
 opts = docopt(__doc__,help=True);
-dump_pickle(opts['<output>'],read(opts['<input>']));
+if opts['--npy'] or (re.search("\.npy$",opts['<output>']) and not opts['--pickle']):
+    np.save(opts['<output>'],read(opts['<input>']));
+else:
+    dump_pickle(opts['<output>'],read(opts['<input>']));
