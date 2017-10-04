6aba21278d4f55274c9244143cbed3f090cc94bf
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-10-04 02:32:04 -0400

    fixing the scripts

--- pmovie/scan/Escan-with-region.py
+++ pmovie/scan/Escan-with-region.py
@@ -3,5 +3,5 @@ minE=0.5;
 maxX=-15e-4;
 def f(frame):
     data=frame['data'];
-    E   = (np.sqrt(data['ux']**2+data['uz']**2+1)-1)*0.511;
+    E   = (np.sqrt(data['ux']**2+data['uy']**2+data['uz']**2+1)-1)*0.511;
     return np.logical_and(E>minE, data['x'] < maxX);
